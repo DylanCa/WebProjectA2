674be4e99286cea27c57fde606b6751a0c45d411
Cedric-M <cirdec95@gmail.com>
2017-04-16 00:53:44 +0100

    Updated login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,15 @@
 @extends('layout')
 
 @section('title')
-    <title>Login</title>
+<title>Login</title>
 @stop
 
 @section('body') 
 
-  
-    <section id="form">
+
+<section id="form">
     <div class="container">
-    <h3>Login:</h3>
+        <h3>Login:</h3>
         <form method="post" action="/login">
             <div class="row">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -25,15 +25,25 @@
                         <input type="password" name="password" class="form-control" required>
                     </div>
                 </div>
+                
             </div>
-        </div> 
-        <button type='submit' class="btn btn-primary">SUBMIT</button>
-    </form>
-    <form method="get" action="create">
-        <button class="btn btn-default">Create an account</button>
-    </form>
-
+            <button type='submit' class="btn btn-primary col-sm-2">SUBMIT</button>
+            
+            
+        </form>
+        <div class="row">
+           <div class="col-sm-6">
+            
+            <form method="get" action="create" class="col-sm-3">
+                <button class="btn btn-default">Create an account</button>
+            </form>            
+        </div>
+        
+        
+    </div>
+</div> 
 </section>
+
 <!-- 
 <div class="container">
     <div class="form-group">
@@ -48,8 +58,7 @@
         </form>
     </div>
 </div>
- --> 
-
+--> 
 @stop
-   
+
 
