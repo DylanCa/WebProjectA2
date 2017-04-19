d2c7a1a3a56556eebd65959dd26004b859842e86
Cedric-M <cirdec95@gmail.com>
2017-04-19 15:29:37 +0100

    Updated store_item

--- resources/views/store_item.blade.php
+++ resources/views/store_item.blade.php
@@ -13,7 +13,7 @@
     
     
     <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-    <link rel="icon" href="images/tab_icon.jpg" sizes="16x16">
+    <link rel="icon" href="/images/tab_icon.jpg" sizes="16x16">
 </head>
 
 <body>
@@ -25,9 +25,9 @@
 						<div class="inner2">
 							<header>
 								<h1>{{$item->name}}</h1>
-								<span class="image main"><img src="/images/store{{$item->id}}.jpg" alt="" /></span>
-								<p>Prix: {{$item->price}}€</p>
-								<p>Description: {{$item->description}}</p>
+								<img src="/images/store{{$item->pictureID}}.jpg" alt="" />
+								<p>{{$item->price}}€</p>
+								<p>{{$item->description}}</p>
 
 							</header>
 							
@@ -36,42 +36,6 @@
 			</div>
 
 
-		<!-- Wrapper -->
-			<div id="wrapper">
-
-
-				<!-- Main -->
-					<div id="main">
-						<div class="inner2">
-							<h1>Generic Page</h1>
-							<span class="image main"><img src="/images/pic13.jpg" alt="" /></span>
-							<p>Some text</p>
-						</div>
-					</div>
-
-
-			</div>
-
-<!-- RIQUE BOUTON-POUSSOIR
-€4,90
-La Brique la plus simple pour comprendre comment fonctionnent les fameuses « briques déclencheuses » ! En associant plusieurs d'entre eux, créer un système logique devient aussi simple qu'efficace : un bouton déclenche une action, l'autre l’arrête. Seul, il peut allumer et éteindre une LED sans la moindre difficulté.
-
-Quelques idées de créations possibles grâce à la brique Bouton :
-
-· Le jeu du buzzer
-· Un jeu de réflexe
-· Des dizaines de jeux vidéos
-· Un piano
-MADE IN FRANCE - Fabriqué à Toulouse, avec passion
-
-Garantie 1 an - Expédié en 24h - Conseil et assistance illimités
- -->
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
 
 @include('sidebar');
 
